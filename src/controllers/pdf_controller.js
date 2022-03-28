cb9e939509885048e3769b1e784bb1c89e7a81bb
Lopez <lopez@MacBook-Pro-de-Lopez.local>
2022-03-28 14:19:39 -0500

    test heroku ok

--- src/controllers/pdf_controller.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const PDFDocument = require('pdfkit');
-function buildPdf(dataCallback, endCallback) {
-    const doc = new PDFDocument();
-    doc.on('data', dataCallback);
-    doc.on('end', endCallback);
-    doc.end();
-}
-module.exports = { buildPdf };
